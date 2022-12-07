c4bcbf77a4678984d163a0afc45e85cb09b9ba69
Victor Muljo <muljo@usc.edu>
2022-12-07 12:44:11 -0800

    Update edit-workout.php

--- ajax-backend/edit-workout.php
+++ ajax-backend/edit-workout.php
@@ -5,6 +5,7 @@ if (!isset($_POST['name']) || trim($_POST['name']) == '' ||
 	!isset($_POST['length']) || trim($_POST['length']) == ''){
 		$error = "<em>Please fill out all the required fields.</em>";
 }else{
+	session_start();
     require '../config/config.php';
 
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -20,6 +21,8 @@ if (!isset($_POST['name']) || trim($_POST['name']) == '' ||
     $date = "'" . $_POST['date'] . "'";
     $length = "'" . $_POST['length'] . "'";
     $id = $_POST['workout_id'];
+	$user_id = 1;
+	// $user_id = $_SESSION['user_id'];
 
     $sql = "UPDATE workouts SET name = $name, date = $date, length = $length WHERE id = $id;";
     $results = $mysqli->query($sql);
@@ -31,7 +34,7 @@ if (!isset($_POST['name']) || trim($_POST['name']) == '' ||
         exit();
     }
 
-    $sql_return = "SELECT * FROM workouts WHERE id = $id and user_id = 1;";
+    $sql_return = "SELECT * FROM workouts WHERE id = $id and user_id = $user_id;";
     $results_return = $mysqli->query($sql_return);
 
     if (!$results_return) {
